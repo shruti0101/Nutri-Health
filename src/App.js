743351e96145168d9464ae1e65d45eec21aca392
shruti0101 <shrutiguptacu@gmail.com>
2024-09-24 14:23:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Layout
+import Layout from "./Layout";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+
+const App = () => {
+  return (
+    <Router>
+    <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+          
+          </Route>
+        </Routes>
+        </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function Home() {
+  return (
+    <>
+  <div>Hello This is home</div>
+   
+   
+    
+    </>
+  )
+}
+
+export default Home;
